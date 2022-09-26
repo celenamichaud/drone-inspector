e75d4c9d57a4222a46883e13206b2f47941757ad
celenamichaud <celena.michaud@uga.edu>
2022-09-26 19:29:16 -0400

    basic serial/deserialize with new drone prefab

--- DroneInspector/Assets/Scripts/DronePose.cs
+++ DroneInspector/Assets/Scripts/DronePose.cs
@@ -3,19 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 public class DronePose : MonoBehaviour
 {
-    // consider creating functions here to return the data to the serializer
-
-    public DronePose()
-    {
-        
-    }
-
-    public DronePose(float[] pos, float[] dir, string name, string type)
-    {
-        DronePose dp = new();
-        dp.transform.localPosition = new Vector3(pos[0], pos[1], pos[2]);
-        // how to handle direction
-        dp.name = name;
-        // type will be saved into private variable
-    }
+    public string actionType;
 }
--- DroneInspector/Assets/Scripts/PoseLoader.cs
+++ DroneInspector/Assets/Scripts/PoseLoader.cs
@@ -7,12 +7,14 @@ public class PoseLoader : MonoBehaviour
 {
     List<DronePose> poses = new List<DronePose>();
     public DronePose posePrefab;
+    public GameObject subject; // the object of interest to the drone
     public class PoseJSON
 	{
         public float[] position;
         public float[] direction;
         public string name;
-        public string type;
+        public string actionType;
+        // create variables to hold drone camera position, and photo subject
 	}
 
     public class PathJSON
@@ -41,34 +43,33 @@ public class PoseLoader : MonoBehaviour
         
     }
 
-    public void addPose(DronePose t)
-	{
-        poses.Add(t);
-	}
-
     public void save()
 	{
-
         PathJSON path = new PathJSON();
         path.poses = new List<PoseJSON>();
-        foreach(DronePose dp in poses)
+        poses = new List<DronePose>(GameObject.FindObjectsOfType<DronePose>());
+        
+        foreach (DronePose dp in poses)
 		{
             PoseJSON pj = new PoseJSON();
+
             Vector3 v = dp.transform.position;
             pj.position = new float[3] { v.x, v.y, v.z };
+            
             Vector3 d = dp.transform.forward;
             pj.direction = new float[3] { d.x, d.y, d.z };
 
             pj.name = dp.name;
-            pj.type = "photo";
+            
+            pj.actionType = "photo";
+            
             path.poses.Add(pj);
         }
 
-
         File.WriteAllText(Application.streamingAssetsPath + "/save.json", JsonConvert.SerializeObject(path));
     }
-	public void load() {
-        
+	public void load() 
+    {
         DronePose[] poses = GameObject.FindObjectsOfType<DronePose>();
         foreach(DronePose dp in poses)
 		{
@@ -80,8 +81,10 @@ public class PoseLoader : MonoBehaviour
         foreach(PoseJSON pj in path.poses) {
             DronePose dp =  Instantiate(posePrefab);
             dp.transform.position = new Vector3(pj.position[0], pj.position[1], pj.position[2]);
+            dp.transform.forward = new Vector3(pj.direction[0], pj.direction[1], pj.direction[2]);
+            dp.name = pj.name;
+            dp.actionType = pj.actionType;
             this.poses.Add(dp);
         }
-        //instantiate objects
     }
 }
