87758dd973ee096263f9784d5062c6b167823d37
celenamichaud <celena.michaud@uga.edu>
2022-12-06 01:08:27 -0500

    Fixed quad angling

--- DroneInspector/Assets/DynamicPoseView.cs
+++ DroneInspector/Assets/DynamicPoseView.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class DynamicPoseView : MonoBehaviour
 {
-    public Transform player;
+    // public Transform player;
+    Vector3 playerPosition;
+    bool updatedPosition = false;
+    GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
         // Required to ensure drone pose prefab can find VR player to face quad view towards
-        GameObject[] cameras = GameObject.FindGameObjectsWithTag("Main Camera");
-        if (player == null && cameras != null)
-        {
-            player = cameras[0].transform;
-        }
+        //GameObject[] cameras = GameObject.FindGameObjectsWithTag("MainCamera");
+        //Debug.Log("there are " + cameras.Length);
+        //GameObject camera = GameObject.Find("Main Camera");
+        //player = cameras[0].transform;
+        
+        //if (player == null && cameras != null)
+        //{
+        //    player = camera.transform;
+        //    Debug.Log("Set player to " + player.position.x + " " + player.position.y + " " + player.position.z);
+        //}
     }
 
     // Update is called once per frame
     void Update()
-    {
-        Vector3 povToPlayer = gameObject.transform.position - player.transform.position;
+    {        
+        if (updatedPosition == false)
+        {
+            GameObject[] cameras = GameObject.FindGameObjectsWithTag("MainCamera");
+            camera = cameras[0];
+            
+            updatedPosition = true;
+        }
+        playerPosition = camera.transform.position;
+        Vector3 povToPlayer = gameObject.transform.position - playerPosition;
         gameObject.transform.forward = povToPlayer;
     }
 }
